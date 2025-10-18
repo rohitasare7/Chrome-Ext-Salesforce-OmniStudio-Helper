57d4de359eb9d6f15a04548afa86d414ca3388fc
Rohitkumar Asare <rohitasare7@gmail.com>
2025-10-19 11:49:53 +1300

    Updated OmniStudio Standard Links

--- src/assets/globalUtil.js
+++ src/assets/globalUtil.js
@@ -26,30 +26,30 @@ export const extractValue = (url) => {
 
     return null;
 }
- 
+
 const directLinkList = [
     {
         id: 1,
         obj: 'OmniScript',
-        omnistudio: `.vf.force.com/lightning/cmp/omnistudio__OmniDesignerAuraWrapper?c__recordId=`,
+        omnistudio: `.lightning.force.com/builder_omnistudio/omnistudioBuilder.app?type=omniscript&id=`,
         vlocity_cmt: `.vf.force.com/lightning/cmp/vlocity_cmt__OmniDesignerAuraWrapper?c__recordId=`,
     },
     {
         id: 2,
         obj: 'IntegrationProcedure',
-        omnistudio: `.vf.force.com/apex/omnistudio__integrationproceduredesigner?id=`,
+        omnistudio: `.lightning.force.com/builder_industries_interaction_rule/industriesBuilder.app?fileType=IntegrationProcedure&fileName=`,
         vlocity_cmt: `.vf.force.com/apex/vlocity_cmt__integrationproceduredesigner?id=`,
     },
     {
         id: 3,
         obj: 'FlexCard',
-        omnistudio: `/lightning/r/OmniUiCard/`,
+        omnistudio: `.lightning.force.com/builder_omnistudio/omnistudioBuilder.app?type=flexcard&id=`,
         vlocity_cmt: `/lightning/r/vlocity_cmt__VlocityCard__c/`,
     },
     {
         id: 4,
         obj: 'DataRaptor',
-        omnistudio: `.vf.force.com/apex/omnistudio__drmapper?id=`,
+        omnistudio: `.lightning.force.com/builder_omnistudio/omnistudioBuilder.app?type=dataraptor&id=`,
         vlocity_cmt: `.vf.force.com/apex/vlocity_cmt__drmapper?id=`,
     }
 
@@ -66,7 +66,7 @@ const getRecordLink = (nameSpace, obj) => {
 // Get Org Suffix URL with VF Force
 export const getOrgSuffixURL = (orgIdentifier, recId, type, sfHostURL, savedNamespace) => {
     let sandboxSuffix = '';
-    let orgNS = 'vlocity-cmt'; //domain suffix namespace
+    let orgNS = 'omnistudio'; //domain suffix namespace
 
     //check if its sandbox
     let isSandbox = localStorage.getItem(sfHostURL + "_" + 'isSandbox') == 'false' ? false : true;
@@ -74,17 +74,18 @@ export const getOrgSuffixURL = (orgIdentifier, recId, type, sfHostURL, savedName
         sandboxSuffix = '.sandbox';
     }
     //Setting namespace for omnistudio
-    if (savedNamespace && savedNamespace == 'omnistudio') {
-        orgNS = 'omnistudio';
+    if (savedNamespace && savedNamespace == 'vlocity_cmt') {
+        orgNS = 'vlocity-cmt';
     }
 
     switch (type) {
         case 'OmniScript':
         case 'DataRaptor':
-        case 'IntegrationProcedure':
-            return `https://${orgIdentifier}--${orgNS}${sandboxSuffix}${getRecordLink(savedNamespace, type)}${recId}`;
         case 'FlexCard':
-            return `https://${sfHostURL}${getRecordLink(savedNamespace, type)}${recId}/view`;
+            return `https://${orgIdentifier}${sandboxSuffix}${getRecordLink(savedNamespace, type)}${recId}`;
+        case 'IntegrationProcedure':
+            // return `https://${sfHostURL}${getRecordLink(savedNamespace, type)}${recId}/view`;
+            return `https://${orgIdentifier}${sandboxSuffix}${getRecordLink(savedNamespace, type)}${recId}`;
         default:
             return null;
     }
@@ -93,6 +94,7 @@ export const getOrgSuffixURL = (orgIdentifier, recId, type, sfHostURL, savedName
 export const getSalesforceURL = (orgIdentifier, sfHostURL, recId, type) => {
     // console.log('sfHostURL ==> ' + sfHostURL);
     const savedNamespace = localStorage.getItem(sfHostURL + "_" + 'ns');
+    console.log('getOrgSuffixURL --> ' + getOrgSuffixURL(orgIdentifier, recId, type, sfHostURL, savedNamespace));
     return getOrgSuffixURL(orgIdentifier, recId, type, sfHostURL, savedNamespace);
 };
 
